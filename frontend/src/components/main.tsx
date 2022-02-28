8ca865ebbedf122481e36775b91c4fc3a64721c6
bloatoo <bloatoo@tuta.io>
2022-02-28 23:16:39 +0200

    feat(frontend): dark mode

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -2,11 +2,41 @@ import styles from '../styles/main.module.css';
 import TaskList from './tasklist';
 import Navbar from './navbar';
 import Timer from './timer';
+import { useState, useEffect } from 'react';
+
+const setVar = (key: string, value: string) => {
+  document.documentElement.style.setProperty(key, value);
+}
+
+const switchColors = (darkMode: boolean) => {
+  if(darkMode) {
+    setVar('--background', '#141414');
+    setVar('--inputBackground', '#191919');
+    setVar('--placeholder', '#999');
+    setVar('--foreground', '#fff');
+    setVar('--lightForeground', '#aaa');
+    setVar('--inverseForeground', '#fff');
+    setVar('--taskListBackground', '#121212');
+    setVar('--border', '#181818');
+    setVar('--hoverBackground', '#181818');
+    setVar('--buttonBackground', 'orange');
+  } else {
+    setVar('--background', '#fff');
+    setVar('--inputBackground', '#fff');
+    setVar('--foreground', '#1d1f21');
+    setVar('--lightForeground', '#aaa');
+    setVar('--inverseForeground', '#fff');
+    setVar('--taskListBackground', '#fbfbfb');
+    setVar('--border', '#eaeaea');
+    setVar('--hoverBackground', '#f9f9f9');
+    setVar('--buttonBackground', 'blue');
+  }
+}
 
 const Main: React.FC = () => {
   return (
     <div>
-      <Navbar />
+      <Navbar onDarkMode={switchColors} />
       <div className={styles.mainContainer}>
         <div className={styles.placeholder}>
           <Timer initialSeconds={25*60} />
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,10 +1,29 @@
 import styles from '../styles/navbar.module.css';
+import { useState, useEffect } from 'react';
 
-function Navbar() {
+interface Props {
+  onDarkMode: (darkMode: boolean) => any;
+}
+
+const Navbar: React.FC<Props> = ({ onDarkMode }) => {
+  let [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const json = JSON.parse(localStorage.getItem("site-dark-mode")!);
+
+    if(json !== null) {
+      setDarkMode(json);
+    }
+  }, []);
+
   return (
     <div className={styles.navbar}>
       <div></div>
       <div className={styles.items}>
+          <a onClick={() => {
+              onDarkMode(!darkMode);
+              setDarkMode(prev => !prev);
+          }}>{ darkMode ? "Light Mode" : "Dark Mode" }</a>
       </div>
     </div>
   )
